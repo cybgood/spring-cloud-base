0c8056d51e9bc8594e6eb86ff13f4b5e6ff85b2f
陈彦斌 <472776879@qq.com>
2020-07-27 01:07:22 +0800

    20200727

--- eureka-server/src/main/java/com/cyb/eureka/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/cyb/eureka/server/EurekaServerApplication.java
@@ -1,13 +1,14 @@
 package com.cyb.eureka.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Application {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- hello/src/main/java/com/cyb/hello/controller/HelloController.java
+++ hello/src/main/java/com/cyb/hello/controller/HelloController.java
@@ -1,26 +1,27 @@
 package com.cyb.hello.controller;
 
+import com.netflix.discovery.EurekaClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.netflix.eureka.CloudEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.ws.Service;
 import java.util.List;
 
 @RestController
 public class HelloController {
-
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @Autowired
-    private DiscoveryClient discoveryClient;
-    @Autowired
-    private Registration registration;
+    private CloudEurekaClient discoveryClient;
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index(){
-        List<ServiceInstance> serviceInstances = discoveryClient.getInstances(registration.getServiceId());
-
+        ServiceInstance instance = discoveryClient.getInstanceInfo();
+        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
         return "Hello World";
     }
 }
